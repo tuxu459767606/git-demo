404c99508acf39fdc445467f5e4676801f57064a
tuxu <459767606@qq.com>
2021-12-15 22:36:36 +0800

    first commit

---
+++ src/main/java/Tt.java
@@ -0,0 +1,42 @@
+/**
+ * 　　　　　　　　┏┓　　　┏┓+ +
+ * 　　　　　　　┏┛┻━━━┛┻┓ + +
+ * 　　　　　　　┃　　　　　　　┃
+ * 　　　　　　　┃　　　━　　　┃ ++ + + +
+ * 　　　　　　 ████━████ ┃+
+ * 　　　　　　　┃　　　　　　　┃ +
+ * 　　　　　　　┃　　　┻　　　┃
+ * 　　　　　　　┃　　　　　　　┃ + +
+ * 　　　　　　　┗━┓　　　┏━┛
+ * 　　　　　　　　　┃　　　┃
+ * 　　　　　　　　　┃　　　┃ + + + +
+ * 　　　　　　　　　┃　　　┃
+ * 　　　　　　　　　┃　　　┃ +
+ * 　　　　　　　　　┃　　　┃
+ * 　　　　　　　　　┃　　　┃　　+
+ * 　　　　　　　　　┃　 　　┗━━━┓ + +
+ * 　　　　　　　　　┃ 　　　　　　　┣┓
+ * 　　　　　　　　　┃ 　　　　　　　┏┛
+ * 　　　　　　　　　┗┓┓┏━┳┓┏┛ + + + +
+ * 　　　　　　　　　　┃┫┫　┃┫┫
+ * 　　　　　　　　　　┗┻┛　┗┻┛+ + + +
+ * 神兽保佑,代码无bug
+ *
+ * @author: tuxu
+ * @date: 2021/12/15 12:32 下午
+ * @describe:
+ * @vision
+ */
+public class Tt {
+
+    /**
+     * @title
+     * @description
+     * @updateTime
+     * @throws
+     */
+    public String changeMethodName (String id){
+        return "hello";
+
+    }
+}
